2c734a51b2dc032b282dcd278bcd4f1b6f84f65c
DESKTOP-BNIS1T3\user <test@test.com>
2024-07-08 18:41:23 +0900

    spring_240708

--- src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quiz.lesson07.entity.RecruitEntity;
@@ -19,26 +20,47 @@ public class Lesson07Quiz02RestController {
 	
 	// http://localhost:8080/lesson07/quiz02/1
 	@GetMapping("/1")
-	public List<RecruitEntity> quiz1() {
-		return recruitRepository.findAllById(8);
+	public RecruitEntity quiz02_1() {
+		// id:8
+		return recruitRepository.findById(8).orElse(new RecruitEntity());
 	}
 	
-	// http://localhost:8080/lesson07/quiz02/2
+	// http://localhost:8080/lesson07/quiz02/2?companyId=1
 	@GetMapping("/2")
-	public List<RecruitEntity> quiz2() {
-		return recruitRepository.findAllByCompanyId(1);
+	public List<RecruitEntity> quiz02_2(
+			@RequestParam("companyId") int companyId) {
+		return recruitRepository.findByCompanyId(companyId);
 	}
 	
 	// http://localhost:8080/lesson07/quiz02/3
 	@GetMapping("/3")
-	public List<RecruitEntity> quiz3() {
-		return recruitRepository.findAllByPositionAndType("웹 back-end 개발자", "정규직");
+	public List<RecruitEntity> quiz02_3() {
+		return recruitRepository.findByPositionAndType("웹 back-end 개발자", "정규직");
 	}
 	
 	// http://localhost:8080/lesson07/quiz02/4
 	@GetMapping("/4")
-	public List<RecruitEntity> quiz4() {
-		return recruitRepository.findAllByPositionAndType("웹 back-end 개발자", "정규직");
+	public List<RecruitEntity> quiz02_4() {
+		return recruitRepository.findByTypeOrSalaryGreaterThanEqual("정규직", 9000);
+	}
+	
+	// http://localhost:8080/lesson07/quiz02/5
+	@GetMapping("/5")
+	public List<RecruitEntity> quiz02_5() {
+		return recruitRepository.findTop3ByTypeOrderBySalaryDesc("계약직");
+	}
+	
+	// http://localhost:8080/lesson07/quiz02/6
+	@GetMapping("/6")
+	public List<RecruitEntity> quiz02_6() {
+		return recruitRepository.findByRegionAndSalaryBetween("성남시 분당구", 7000, 8500);
+	}
+	
+	// http://localhost:8080/lesson07/quiz02/7
+	@GetMapping("/7")
+	public List<RecruitEntity> quiz02_7() {
+//		마감일이 2026-04-10 이후이고 연봉이 8100 이상인 정규직 공고를 연봉 내림차순으로 조회하세요.
+		return recruitRepository.findByDeadlineAndSalaryAndType("2026-04-10", 8100, "정규직");
 	}
 	
 }
--- src/main/java/com/quiz/lesson07/respository/RecruitRepository.java
+++ src/main/java/com/quiz/lesson07/respository/RecruitRepository.java
@@ -3,13 +3,34 @@ package com.quiz.lesson07.respository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.quiz.lesson07.entity.RecruitEntity;
 
 public interface RecruitRepository extends JpaRepository<RecruitEntity, Integer>{
 	
-	public List<RecruitEntity> findAllById(int id);
-	public List<RecruitEntity> findAllByCompanyId(int id);
-	public List<RecruitEntity> findAllByPositionAndType(String position, String type);
+	// JPQL => Entity에 조회
+	// quiz02 2번
+	public List<RecruitEntity> findByCompanyId(int companyId);
+	//3번
+	public List<RecruitEntity> findByPositionAndType(String position, String type);
+	//4번
+	public List<RecruitEntity> findByTypeOrSalaryGreaterThanEqual(String type, int salary);
+	//5번
+	public List<RecruitEntity> findTop3ByTypeOrderBySalaryDesc(String type);
+	//6번
+	public List<RecruitEntity> findByRegionAndSalaryBetween(String region, int startSalary, int endSalary);
 
+	//7번 - native query (DB에 직접 SQL)
+//	마감일이 2026-04-10 이후이고 연봉이 8100 이상인 정규직 공고를 연봉 내림차순으로 조회하세요.
+	@Query(value="select * from `recruit` "
+			+ "where `deadline` >= : deadline "
+			+ "and `salary` > : salary "
+			+ "and `type` = : type "
+			+ "order by `salary` desc", nativeQuery = true)
+	public List<RecruitEntity> findByDeadlineAndSalaryAndType(
+			@Param("deadline") String deadline,
+			@Param("salary") int salary,
+			@Param("type") String type);
 }
